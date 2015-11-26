6cbb0af4ef2e028c7d4aa47b9142c8258ba70668
Alejandro Conde <adcs2008@gmail.com>
2015-11-26 09:11:46 -0430

    Cambios en la BD

--- db/schema.rb
+++ db/schema.rb
@@ -137,29 +137,29 @@ ActiveRecord::Schema.define(version: 20150624164848) do
   add_index "sells", ["usuario_id"], name: "index_sells_on_usuario_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",          limit: 255
+    t.string   "email"
     t.date     "fecha_registro"
-    t.string   "password",       limit: 255
-    t.string   "username",       limit: 255
+    t.string   "password"
+    t.string   "username"
     t.integer  "id_user"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "usuarios", force: :cascade do |t|
-    t.string   "email",                  limit: 255
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email"
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "permission_level",       limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.string   "permission_level"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "authentication_token"
   end
 
